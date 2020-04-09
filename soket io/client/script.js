c498f6113bebb95c1aae0833498139b318709b87
Ego <Ego@DESKTOP-UVDH5VA>
2020-04-09 20:18:54 +0200

    09.04.2020

---
+++ soket io/client/script.js
@@ -0,0 +1,12 @@
+$(function () {
+    var socket = io();
+    socket.on('ServerMessage', function (message) {
+        $('.chat').append($('<p>').text(message));
+    });
+    $('form').submit(function (e) {
+        e.preventDefault(); // prevents page reloading
+        socket.emit('ClientMessage', $('#message').val());
+        $('#message').val('');
+        return false;
+    });
+});
