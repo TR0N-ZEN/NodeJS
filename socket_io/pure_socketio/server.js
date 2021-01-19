9801ba6d310b6cd819a12f79824fde286a9f8b3a
Theo Reichert <theo.reichert7@gmail.com>
2021-01-19 05:59:57 +0100

    restructured and starting with node module cluster

---
+++ socket_io/pure_socketio/server.js
@@ -0,0 +1,30 @@
+const { Server } = require('socket.io');
+const io = new Server({
+    path: "/socket.io",
+    serveClient: false,
+    //below are Engine.io options
+    pingTimeout: 5000,
+    pingInterval: 10000,
+    transports: ["websocket"],
+    cookie: true
+});
+
+const IPaddress = 'localhost';
+const port = 80;
+io.listen()
+
+io.of
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('ClientMessage', function (message) {
+        console.log('client: ' + message);
+        io.emit('ServerMessage', message);
+    });
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+httpserver.listen(port, IPaddress, function () {
+    console.log( 'Server is listening on ' + IPaddress + ':' + port.toString() );
+});
