75298659f7e7dfbc2c7344e1e25e2466b29a69b3
Theo Reichert <theo.reichert7@gmail.com>
2021-03-15 01:25:15 +0100

    dunno what actually changed

--- websockets/server.js
+++ websockets/server.js
@@ -1,14 +1,16 @@
-const WebSocket = require("ws");
+const WebSocket = require("ws"); //the module "ws" (standing for WebSockets) is not part of node's standard repository so it has to be installed via the command 'npm install ws'
 
+// Websockets 
 let options = {
     port: 3021
 };
 const wss = new WebSocket.Server(options);// 'wss' is the WebSocketServer
 let clients = new Set();
 
-wss.on("connection", ws => { //'ws' is the WebSocket instance
+wss.on("connection", ws => { //'ws' is the WebSocket that is given to the callback function on a connection to the 'wss' - WebSocketServer
     console.log("New client connected");
     clients.add(ws);
+		//establishing event listeners for the 'ws' - WebSocket
     ws.on("message", message => {
         console.log(typeof(message));
         console.log(`${message}`);
